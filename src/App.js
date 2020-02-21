08e188e5aae781eed18a18cc8890875ff1e39776
Alfan Zain <alfanzainkp@gmail.com>
2020-02-21 20:16:17 +0700

    [2.2] React Context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ctxColor = React.createContext();
+
+/*
+Dengan React Context:
+- Kita tidak perlu passing props secara berantai.
+- Kita hanya perlu memberikan context kepada component yang membutuhkan. Apalagi terhadap child component.
+*/
+
+const App = () => {
+	const [lang, setLang] = useState('Indonesia');
+
+	return (
+		<ctxColor.Provider value={lang}>
+			<div style={{ margin: '20px auto', width: '500px', textAlign: 'center' }}>
+				<Welcome />
+				<Form />
+			</div>
+		</ctxColor.Provider>
+	);
+};
+
+const Welcome = () => {
+	return (
+		<ctxColor.Consumer>
+			{context => (
+				<div>
+					<h3>Selamat datang!</h3>
+					<br />
+					<p>
+						Anda berada pada portal berbahasa <strong>{context}</strong>
+					</p>
+				</div>
+			)}
+		</ctxColor.Consumer>
+	);
+};
+
+const Form = () => {
+	return (
+		<div>
+			<Confirm />
+		</div>
+	);
+};
+
+const Confirm = () => {
+	return (
+		<ctxColor.Consumer>
+			{context => (
+				<div>
+					<button>
+						{context == 'Indonesia' && 'Siapa nama kamu?'}
+						{context == 'English' && 'Who are you?'}
+					</button>
+				</div>
+			)}
+		</ctxColor.Consumer>
+	);
+};
 
 export default App;
