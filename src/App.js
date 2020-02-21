3d094d36108a94b77240a388bdddcfac3e5065c2
Alfan Zain <alfanzainkp@gmail.com>
2020-02-21 23:52:53 +0700

    [2.4] Higher-Order Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const withUser = WrappedComponent => {
+	const WithUser = () => (
+		<WrappedComponent user="Alfan Zain"></WrappedComponent>
+	);
+	return WithUser;
+};
 
-export default App;
+const App = props => {
+	return <>You are logged on as {props.user}</>;
+};
+
+export default withUser(App);
