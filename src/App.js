6f23f3a379c8b96997535e7d16609e04a348765a
Alfan Zain <alfanzainkp@gmail.com>
2020-02-22 18:01:58 +0700

    [2.5] HOC localstorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const withUser = WrappedComponent => {
-	const WithUser = () => (
-		<WrappedComponent user="Alfan Zain"></WrappedComponent>
-	);
-	return WithUser;
+const withLocalStorage = WrappedComponent => {
+	const WithLocalStorage = props => {
+		const get = key => localStorage.getItem(key);
+		const save = (key, data) => localStorage.setItem(key, data);
+		const remove = key => localStorage.removeItem(key);
+
+		return (
+			<WrappedComponent get={get} save={save} remove={remove} {...props} />
+		);
+	};
+
+	return WithLocalStorage;
 };
 
 const App = props => {
-	return <>You are logged on as {props.user}</>;
+	const [name, setName] = useState(props.get('name'));
+
+	useEffect(() => {
+		if (!name) {
+			console.log(name);
+			props.save('name', 'guest');
+			setName('guest');
+		}
+	}, [name]);
+
+	return <>You are logged on as {name}</>;
 };
 
-export default withUser(App);
+export default withLocalStorage(App);
